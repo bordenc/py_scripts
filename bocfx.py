d11021c2460cf5781b71530a70fbcceef30999bf
Borden <bordenc@users.noreply.github.com>
2022-12-10 00:18:26 -0500

    Changed bocfx.py to use requests library

--- bocfx.py
+++ bocfx.py
@@ -6,15 +6,14 @@ Documentation at https://www.bankofcanada.ca/valet/docs
 
 import datetime
 import decimal
-import json
-import urllib.request
+import requests
 
 
 def get_fx(from_currency='CAD', to_currency='CAD',
-           start_date=None, end_date=None):
+           start_date=datetime.date.today(), end_date=datetime.date.today()):
     """
-    Return the exchange to another currency expressed from a base currency
-    on a date or range of dates.
+    Get the exchange to another currency expressed from a base currency for
+    the provided date range.
 
     :param from_currency: The base currency using 3-digit ISO 4217 currency
         code.
@@ -26,11 +25,10 @@ def get_fx(from_currency='CAD', to_currency='CAD',
         of exchange rates.
 
     :return: A dict of exchange rates with key=date (str) and
-        value=exchange rate (decimal.Decimal).
+        value=exchange rate (:py:class:`decimal.Decimal`).
 
     TODO: add unit tests to see if I did this correctly
     """
-
     # Match BOC precision
     decimal.getcontext().prec = 5
 
@@ -40,28 +38,17 @@ def get_fx(from_currency='CAD', to_currency='CAD',
     if from_currency == to_currency:
         return decimal.Decimal(1)
 
-    if not end_date:
-        start_date = end_date
-    if not start_date:
-        end_date = start_date
-
     if from_currency == 'CAD' or to_currency == 'CAD':
 
         series_name = 'FX' + from_currency + to_currency
         url = ('https://www.bankofcanada.ca/valet/observations/'
                + series_name + '/json')
 
-        if start_date:
-            start_date = datetime.date.fromisoformat(start_date)
-            end_date = datetime.date.fromisoformat(end_date)
-                
-            query = ('?start_date=' + str(start_date)
-                     + '&end_date=' + str(end_date))
-        else:
-            query = '?recent=1'
+        query = {'start_date': str(start_date),
+                 'end_date': str(end_date)}
 
-        with urllib.request.urlopen(url + query) as file:
-            json_data = json.load(file)
+        with requests.get(url, params=query) as file:
+            json_data = file.json()
 
             fx_rates = {item['d']: decimal.Decimal(item[series_name]['v'])
                         for item in json_data['observations']}
@@ -78,15 +65,13 @@ def get_fx(from_currency='CAD', to_currency='CAD',
     if len(fx_rates) > 1:
         return fx_rates
     if len(fx_rates) == 1:
-        # TODO: Make this return one value without breaking other things
-        return fx_rates
+        return fx_rates.popitem()[1]
     else:
         # Bank holidays almost never last more than a week,
         # so back up seven days and pop the most recent observation
-        delta = datetime.timedelta(days=7)
-        start_date -= delta
+        start_date -= datetime.timedelta(days=7)
         fx_rates = get_fx(from_currency, to_currency,
-                          str(start_date), str(end_date))
+                          start_date, end_date)
         key = sorted(fx_rates.keys(), reverse=True)[0]
         return {key: fx_rates[key]}
 
@@ -95,21 +80,22 @@ if __name__ == '__main__':
     from argparse import ArgumentParser
     arg_parser = ArgumentParser(description=__doc__)
     arg_parser.add_argument(
-        '--from_currency', default='CAD')
+        '--from-currency', default='CAD')
     arg_parser.add_argument(
-        '--to_currency', default='CAD')
+        '--to-currency', default='CAD')
     arg_parser.add_argument(
-        '--from_date', default=None)
+        '--from-date', default=str(datetime.date.today()))
     arg_parser.add_argument(
-        '--to_date', default=None)
+        '--to-date', default=str(datetime.date.today()))
     args = arg_parser.parse_args()
     results = get_fx(args.from_currency, args.to_currency,
-                     args.from_date, args.to_date)
+                     datetime.date.fromisoformat(args.from_date),
+                     datetime.date.fromisoformat(args.to_date))
 
     if not hasattr(results, 'keys'):
         key = str(args.from_date)
         results = {key: results}
 
-    print('FX 1.00', args.from_currency, '=')
+    print('FX 1.0000', args.from_currency, '=')
     for fx_date in results.keys():
-        print(fx_date, ':', results[fx_date], args.to_currency)
+        print(fx_date, ',', results[fx_date], args.to_currency)
--- rename_files_from_contents.py
+++ rename_files_from_contents.py
@@ -1,19 +1,47 @@
 #! /usr/bin/python3
+"""
+"""
 
 import os
 import re
 from shutil import move
 
-file_name_match_pattern = r''
-in_file_match_pattern = r''
-base_directory = ''
 
-for file in os.scandir(base_directory):
-    if re.search(file_name_match_pattern, file.name):
-        file_contents = open(file, 'r')
-        matches = re.search(in_file_match_pattern,
-                            file_contents.read())
-        if matches:
-            # Need to add something to preserve extensions
-            new_file_name = os.path.join(base_directory, matches.group(1))
+def rename_files_from_contents(
+        file_name_match_pattern,
+        in_file_match_pattern,
+        base_directory):
+    """
+
+
+    :param file_name_match_pattern:
+    :param in_file_match_pattern:
+    :param base_directory:
+
+    :return:
+    """
+
+    for file in os.scandir(base_directory):
+        if re.search(file_name_match_pattern, file.name):
+            file_contents = open(file, 'r')
+            matches = re.search(in_file_match_pattern,
+                                file_contents.read())
+            if matches:
+                # Need to add something to preserve extensions
+                new_file_name = os.path.join(base_directory, matches.group(1))
             move(file, new_file_name)
+
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+    parser = ArgumentParser(description=__doc__)
+    parser.add_argument('file_name_match_pattern')
+    parser.add_argument('in_file_match_pattern')
+    parser.add_argument('base_directory')
+    args = parser.parse_args()
+
+    rename_files_from_contents(
+        args.file_name_match_pattern,
+        args.in_file_match_pattern,
+        args.base_directory
+    )
